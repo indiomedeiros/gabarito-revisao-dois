70e4e05bf15bf008f00cd6368a7f50747486b543
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-04-02 09:52:31 -0300

    Resolve questão 7

--- condicionais.py
+++ condicionais.py
@@ -8,3 +8,14 @@ def verificar_maioridade():
         print("Menor de idade")
 
 verificar_maioridade()
+
+def verifica_par():
+    print("======================= VERIFICA PAR =======================")
+
+    numero = int(input("Digite um número: "))
+    if(numero % 2 == 0):
+           print("O número é par!")
+    else:
+        print("O número é ímpar!")
+
+verifica_par()

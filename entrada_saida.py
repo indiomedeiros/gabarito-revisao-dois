add95fc99ae75762f67a14247f03735ce57ceb77
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-04-02 09:39:28 -0300

    Resolve questão 1

---
+++ entrada_saida.py
@@ -0,0 +1,6 @@
+def saudar_usuario():
+    print("======================= SAUDAÇÃO =======================")
+    nome = input("Qual é o seu nome? ")
+    print("Olá, " + nome + "! Seja bem-vindo(a)!")
+
+saudar_usuario()

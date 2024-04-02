0b6a62b9ecb1d218b3e8fc3d141be40bf70033db
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-04-02 09:41:06 -0300

    Resolve questão 3

--- entrada_saida.py
+++ entrada_saida.py
@@ -13,3 +13,12 @@ def somar_numeros():
     print("A soma dos números é: " + str(soma))
 
 somar_numeros()
+
+def multiplicar_numeros():
+    print("======================= MULTIPLICAÇÃO =======================")
+    primeiro_numero = float(input("Digite um numero: "))
+    segundo_numero = float(input("Digite segundo número: "))
+    multiplcacao = primeiro_numero * segundo_numero
+    print("O produto dos números é", multiplcacao)
+
+multiplicar_numeros()
